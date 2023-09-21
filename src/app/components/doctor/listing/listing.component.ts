e46047da1a04c6015255f5a0bf1f4e2ff263973f
Muhammad Awais <muhammad.awais@shipsta.com>
2023-09-21 23:45:40 +0500

    doctor listing ui done

--- src/app/components/doctor/listing/listing.component.ts
+++ src/app/components/doctor/listing/listing.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingComponent implements OnInit {
 
+  doctors: any = [
+    {
+      img: "../../../../assets/img/d2.jpg",
+      name: "Robert Cris",
+      field: "Neurologist",
+      education: "PHD, MBBS, FPS, CPS",
+      rating: "4.8"
+    },
+    {
+      img: "../../../../assets/img/d1.jpg",
+      name: "Noel Haueg",
+      field: "Otorhinolaryngology",
+      education: "MD, PHD, MBBS, FPS, CPS",
+      rating: "4.8"
+    },
+    {
+      img: "../../../../assets/img/d3.jpg",
+      name: "Kathryn Brawl",
+      field: "Psychiatrist",
+      education: "PHD, MBBS, FPS, CPS",
+      rating: "4.1"
+    },
+    {
+      img: "../../../../assets/img/d4.jpg",
+      name: "Tanya Sale",
+      field: "Endocrinologist",
+      education: "MD, MBBS, FPS, CPS",
+      rating: "5.0"
+    },
+    {
+      img: "../../../../assets/img/d5.jpg",
+      name: "Shaffer Rawl",
+      field: "Cardiologist",
+      education: "MBBS, FPS, CPS",
+      rating: "4.8"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
