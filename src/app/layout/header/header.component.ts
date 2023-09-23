5949cc1289c8cbc2fc2a8a075b46356cc0c7e684
Muhammad Awais <muhammad.awais@shipsta.com>
2023-09-23 23:49:26 +0500

    header added

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
+import { Component, HostListener, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  toggleButton: boolean = false;
+  sidebar: boolean = false;
+  fadeSection: boolean = false;
+  backBtnState: boolean = false;
 
-  ngOnInit(): void {
+  routes: any = [
+    { name: "Home", path: '/'},
+    { name: "About", path: '/'},
+    { name: "Finances", path: '/'},
+    { name: "Insurance", path: '/'},
+  ]
+
+  constructor(
+    private location: Location,
+  ) {}
+
+  toggleMenu() {
+    // animating icon
+    this.toggleButton = !this.toggleButton;
+
+    // animating sidebar
+    this.sidebar = !this.sidebar;
+
+    // enabling fade section 
+    this.fadeSection = !this.fadeSection;
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
