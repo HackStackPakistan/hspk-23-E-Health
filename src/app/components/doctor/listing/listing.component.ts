834976237a65d842d108908ba30bd3c8e9654233
Ilyas Ahmed <ilyasahmed081@gmail.com>
2023-09-27 00:33:40 +0500

    status icon added in patient and doctor screens

--- src/app/components/doctor/listing/listing.component.ts
+++ src/app/components/doctor/listing/listing.component.ts
@@ -13,35 +13,40 @@ export class ListingComponent implements OnInit {
       name: "Dr. Robert Cris",
       field: "Neurologist",
       education: "PHD, MBBS, FPS, CPS",
-      rating: "4.8"
+      rating: "4.8",
+      status: 'upcoming'
     },
     {
       img: "../../../../assets/img/d1.jpg",
       name: "Dr. Noel Haueg",
       field: "Otorhinolaryngology",
       education: "MD, PHD, MBBS, FPS, CPS",
-      rating: "3.9"
+      rating: "3.9",
+      status: 'upcoming'
     },
     {
       img: "../../../../assets/img/d3.jpg",
       name: "Dr. Kathryn Brawl",
       field: "Psychiatrist",
       education: "PHD, MBBS, FPS, CPS",
-      rating: "4.1"
+      rating: "4.1",
+      status: 'compeleted'
     },
     {
       img: "../../../../assets/img/d4.jpg",
       name: "Dr. Tanya Sale",
       field: "Endocrinologist",
       education: "MD, MBBS, FPS, CPS",
-      rating: "5.0"
+      rating: "5.0",
+      status: 'compeleted'
     },
     {
       img: "../../../../assets/img/d5.jpg",
       name: "Dr. Shaffer Rawl",
       field: "Cardiologist",
       education: "MBBS, FPS, CPS",
-      rating: "4.8"
+      rating: "4.8",
+      status: 'compeleted'
     },
   ]
 
